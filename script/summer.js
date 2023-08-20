77a7e8bc2997fcf897005e0e0e2eebf788ab4367
Rabiul37 <hmrabiul37@gmail.com>
2023-08-20 21:54:48 +0600

    this is a final commit

--- script/summer.js
+++ script/summer.js
@@ -1,22 +1,43 @@
-let Total = 0 ;
-function card(target){
-    const productName = target.childNodes[3].childNodes[7].innerText ;
-    const productPrice = parseFloat(target.childNodes[3].childNodes[9].innerText);
-    const productNameContainer = document.getElementById('product-Name');
-    const ol =document.createElement('ol');
-    const li = document.createElement('li');
-    li.innerHTML = productName;
-    ol.appendChild(li);
-    productNameContainer.appendChild(ol);
-     Total =  Total + productPrice ;
-    document.getElementById('total').innerText = Total;
-    if(Total > 200){
-    console.log(discount);
-    }
-    
+let Total = 0;
+function card(target) {
+  const productName = target.childNodes[3].childNodes[7].innerText;
+  const productPrice = parseFloat(target.childNodes[3].childNodes[9].innerText);
+  const productNameContainer = document.getElementById("product-Name");
+  const p = document.createElement("p");
+  productNameContainer.appendChild(p);
+  let count = productNameContainer.childElementCount;
+   p.innerHTML = `${count - 1 } . ${productName}`;
   
+ 
+  Total = Total + productPrice;
+  document.getElementById("total").innerText = Total.toFixed(2);
+  document.getElementById("total-bill").innerText = Total.toFixed(2);
+  if (Total >= 200) {
+    document.getElementById("apply").removeAttribute("disabled");
+  } else if (Total > 0) {
+    document.getElementById("btn-purchase").removeAttribute("disabled");
+  }
 }
 
-let main = 500;
-    let discount = main - 30 / 100 * main ;
-    console.log(discount);
+document.getElementById("apply").addEventListener("click", function () {
+  const coupon = document.getElementById("coupon").value;
+  let discountFormula = Total - (20 / 100) * Total;
+  let discount = Total - discountFormula;
+  if (coupon === "SELL200") {
+    document.getElementById("discount").innerText = discount.toFixed(2);
+    document.getElementById("total-bill").innerText =
+      discountFormula.toFixed(2);
+  } else {
+    document.getElementById("total").innerText = Total.toFixed(2);
+    document.getElementById("total-bill").innerText = Total.toFixed(2);
+    alert("Your coupon is incorrect");
+  }
+ coupon = '';
+});
+
+document.getElementById("home").addEventListener("click", function () {
+  // window.open("http://127.0.0.1:5500/index.html");
+  document.getElementById("total").innerText = "00";
+  document.getElementById("total-bill").innerText = "00";
+const productNameContainer = document.getElementById("product-Name").innerHTML= `<h1 class="text-xl font-semibold" >Product List : </h1>`;
+});
